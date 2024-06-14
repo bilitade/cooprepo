8129f5b4fa801a5e6487cc7c93495341e98d9c88
Bilisuma <coop@DIG-BLP-10.coopbank.local>
2024-06-14 22:41:41 +0300

    password functionality with custom template done

--- file_management/urls.py
+++ file_management/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
@@ -14,5 +15,9 @@ urlpatterns = [
     path('download_file/', views.download_file, name='download_file'),
      path('search/', views.search, name='search'),
      path('users/', views.load_users, name="users"),
-  path('', include('django.contrib.auth.urls'))
+
+     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='passwordreset/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='passwordreset/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='passwordreset/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='passwordreset/password_reset_complete.html'), name='password_reset_complete'),
 ]
