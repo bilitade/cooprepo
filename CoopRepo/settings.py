970fb23c32ced1f93858c0e32a0359e32d1809f7
Bilisuma <coop@DIG-BLP-10.coopbank.local>
2024-06-16 16:21:42 +0300

    exteranl ip allowed and small modification on password reset interface

--- CoopRepo/settings.py
+++ CoopRepo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bon32fk-fkg9l!odw-g@c-*sc(t#w^@fvj$c*$w)-s9q6r1!4#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.1.15.132', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['10.1.15.132', 'localhost', '127.0.0.1','192.168.14.59']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
